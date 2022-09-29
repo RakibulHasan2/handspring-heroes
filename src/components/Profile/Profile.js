a809eec1fa879b6ddd4e00bfd4592517450ca732
RakibulHasan2 <rakibulhasan99445@gmail.com>
2022-09-29 11:22:29 +0600

    profile section style done

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -25,6 +25,23 @@ const Profile = () => {
                 <p>Age</p>
                </div>
             </div>
+             <h3>Add A Break</h3>
+            <div className='time-break'>
+                    <button>45s</button>
+                    <button>60s</button>
+                    <button>75s</button>
+                    <button>99s</button>
+            </div>
+            <h3>Exercise-information</h3>
+            <div className='exercise-Time'>
+                    <h4>Exercise Time</h4>
+                    <p>00s</p>
+            </div>
+            <div className='exercise-Time'>
+                    <h4>Break Time</h4>
+                    <p>00s</p>
+            </div>
+            <button className='activity-completed-btn'>See activity</button>
         </div>
     );
 };
