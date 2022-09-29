f8dab728ae7a1726c881fd35aee03e79e24f765a
RakibulHasan2 <rakibulhasan99445@gmail.com>
2022-09-29 13:54:07 +0600

    break time added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,6 +7,24 @@ const Profile = (props) => {
     for(const exerciseTime of addTime){
        totalTime = totalTime + exerciseTime.time
     }
+  
+    const addBreak15 = () => {
+    const time = document.getElementById('15s').innerText
+     document.getElementById('updateTime').innerText = time;
+    }
+    const addBreak30 = () => {
+        const time = document.getElementById('30s').innerText
+         document.getElementById('updateTime').innerText = time;
+        }
+    const addBreak45 = () => {
+            const time = document.getElementById('45s').innerText
+            document.getElementById('updateTime').innerText = time;
+            }
+    const addBreak60 = () => {
+            const time = document.getElementById('60s').innerText
+            document.getElementById('updateTime').innerText = time;
+            }
+
     return (
         <div className='profile'>
             <div className='profile-info'>
@@ -32,19 +50,19 @@ const Profile = (props) => {
             </div>
              <h3>Add A Break</h3>
             <div className='time-break'>
-                    <button>45s</button>
-                    <button>60s</button>
-                    <button>75s</button>
-                    <button>99s</button>
+                    <button id='15s' onClick={addBreak15}>15s</button>
+                    <button id='30s' onClick={addBreak30} >30s</button>
+                    <button id='45s' onClick={addBreak45} >45s</button>
+                    <button id='60s' onClick={addBreak60} >60s</button>
             </div>
             <h3>Exercise-information</h3>
             <div className='exercise-Time'>
                     <h4>Exercise Time</h4>
-                    <p>{totalTime}s</p>
+                    <p>{totalTime} min</p>
             </div>
             <div className='exercise-Time'>
                     <h4>Break Time</h4>
-                    <p>00s</p>
+                    <p id='updateTime'>00s</p>
             </div>
             <button className='activity-completed-btn'>See activity</button>
         </div>
